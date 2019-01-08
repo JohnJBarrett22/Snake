16cb4cd8ba11a4e8901c7585f3fc2313961f8346
John Barrett <johnjbarrett22@gmail.com>
2019-01-08 10:05:26 -0800

    Added collision detection, end game mehanic and game sounds

--- snake.js
+++ snake.js
@@ -42,18 +42,31 @@ let d;
 function direction(event){
     if(event.keyCode == 37 && d != "RIGHT"){
         d = "LEFT";
+        left.play();
     } else if(event.keyCode == 39 && d != "LEFT"){
         d = "RIGHT";
+        right.play();
     } else if(event.keyCode == 38 && d != "DOWN"){
         d = "UP";
+        up.play();
     } else if(event.keyCode == 40 && d != "UP"){
         d = "DOWN";
+        down.play();
     }
+}
+
+function collision(head, array){
+    for(let i = 0; i < array.length; i++){
+        if(head.x == array[i].x && head.y == array[i].y){
+            return true;
+        }
+    }
+    return false;
 }
 
 function draw(){
     ctx.drawImage(ground,0,0);
-    for(let i=0; i < snake.length; i++){
+    for(let i = 0; i < snake.length; i++){
         ctx.fillStyle = ( i == 0 )? "green" : "white";
         ctx.fillRect(snake[i].x, snake[i].y, box, box);
 
@@ -65,16 +78,30 @@ function draw(){
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    snake.pop();
-
     if(d == "LEFT") snakeX -= box;
     if(d == "RIGHT") snakeX += box;
     if(d == "UP") snakeY -= box;
     if(d == "DOWN") snakeY += box;
 
+    if(snakeX == food.x && snakeY == food.y){
+        score++;
+        eat.play();
+        food = {
+            x : Math.floor(Math.random()*17+1) * box,
+            y : Math.floor(Math.random()*15+3) * box
+        }
+    }else{
+        snake.pop();
+    }
+    
     let newHead = {
         x : snakeX,
         y : snakeY
+    }
+
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box || collision(newHead, snake)){
+        clearInterval(game);
+        dead.play();
     }
 
     snake.unshift(newHead);
